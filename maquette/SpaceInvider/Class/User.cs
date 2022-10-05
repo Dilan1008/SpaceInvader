96c92a991082430ef251048d72726e61c40bf91f
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-05 12:11:10 +0200

    Create User.cs

---
+++ maquette/SpaceInvider/Class/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class User
+    {
+        private string userName;
+        private int HighScore;
+        private int Score;
+
+        public User(string userName, int highScore, int score)
+        {
+            this.userName = userName;
+            HighScore = highScore;
+            Score = score;
+        }
+
+        public string UserName { get => userName; set => userName = value; }
+        public int HighScore1 { get => HighScore; set => HighScore = value; }
+        public int Score1 { get => Score; set => Score = value; }
+    }
+}
