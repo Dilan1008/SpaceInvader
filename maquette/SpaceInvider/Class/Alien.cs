d5ca25965c0f69348caec733008034ba73bb9c4b
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-05 12:10:59 +0200

    Update Alien.cs

--- maquette/SpaceInvider/Class/Alien.cs
+++ maquette/SpaceInvider/Class/Alien.cs
@@ -1,7 +1,23 @@
 ﻿namespace Class
 {
-    public class Class1
+    public class Alien
     {
+        private string name;
+        private int lat;
+        private int longitude;
+        private int speed;
 
+        public Alien(string name, int lat, int longitude, int speed)
+        {
+            this.name = name;
+            this.lat = lat;
+            this.longitude = longitude;
+            this.speed = speed;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Lat { get => lat; set => lat = value; }
+        public int Longitude { get => longitude; set => longitude = value; }
+        public int Speed { get => speed; set => speed = value; }
     }
 }
