84b31e6d5e39fd863baba6374b5c524c82a32dfd
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-05 12:11:16 +0200

    Create Life.cs

---
+++ maquette/SpaceInvider/Class/Life.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Life
+    {
+        private int contain;
+
+        public Life(int contain)
+        {
+            this.contain = contain;
+        }
+
+        public int Contain { get => contain; set => contain = value; }
+    }
+}
