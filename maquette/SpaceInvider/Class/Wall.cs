e594e3d5f083908a4a1ae5c32f52f042cfcd816f
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-05 12:11:07 +0200

    Create Wall.cs

---
+++ maquette/SpaceInvider/Class/Wall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Wall
+    {
+        private int lat;
+        private int longitude;
+        private int height;
+        private int width;
+
+        public Wall(int lat, int longitude, int height, int width)
+        {
+            this.lat = lat;
+            this.longitude = longitude;
+            this.height = height;
+            this.width = width;
+        }
+
+        public int Lat { get => lat; set => lat = value; }
+        public int Longitude { get => longitude; set => longitude = value; }
+        public int Height { get => height; set => height = value; }
+    }
+}
