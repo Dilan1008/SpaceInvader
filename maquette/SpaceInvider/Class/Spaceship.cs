5cd1a3391c518cc7486542ddb98feb6c91da0f7a
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-05 12:11:12 +0200

    Create Spaceship.cs

---
+++ maquette/SpaceInvider/Class/Spaceship.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Spaceship
+    {
+        
+        private int speed;
+        private int lat;
+        private int longitude;
+
+        public Spaceship(int speed, int lat, int longitude)
+        {
+            this.speed = speed;
+            this.lat = lat;
+            this.longitude = longitude;
+        }
+
+        public int Speed { get => speed; set => speed = value; }
+        public int Lat { get => lat; set => lat = value; }
+        public int Longitude { get => longitude; set => longitude = value; }
+    }
+}
