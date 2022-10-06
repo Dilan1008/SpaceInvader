9b1aa0a440c5b3aeb761feed18e1b1b6f7eaa1ce
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-06 21:27:26 +0200

    Create Shoot.cs

---
+++ SpaceInvaders2/Class/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Shoot
+    {
+        private int speed;
+        private int lat;
+        private int longitude;
+
+        public Shoot()
+        {
+        }
+
+        public Shoot(int speed, int lat, int longitude)
+        {
+            this.speed = speed;
+            this.lat = lat;
+            this.longitude = longitude;
+        }
+
+        public int Speed { get => speed; set => speed = value; }
+        public int Lat { get => lat; set => lat = value; }
+        public int Longitude { get => longitude; set => longitude = value; }
+    }
+}
