4f63d6c6f81a374a722c86e4aa9550b82a722e66
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-07 22:48:03 +0200

    Create Alien.cs

---
+++ SpaceInvaders/Class/Alien.cs
@@ -0,0 +1,27 @@
+﻿namespace Class
+{
+    public class Alien
+    {
+        private string name;
+        private int lat;
+        private int longitude;
+        private int speed;
+
+        public Alien(string alienName)
+        {
+        }
+
+        public Alien(string name, int lat, int longitude, int speed)
+        {
+            this.name = name;
+            this.lat = lat;
+            this.longitude = longitude;
+            this.speed = speed;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Lat { get => lat; set => lat = value; }
+        public int Longitude { get => longitude; set => longitude = value; }
+        public int Speed { get => speed; set => speed = value; }
+    }
+}
