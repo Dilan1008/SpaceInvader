03a09d1500274c5e0ac0fd05b8bd9f39998260af
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-10 08:45:55 +0200

    Create SpaceShip.cs

---
+++ SpaceInvaders2/Test/SpaceShip.cs
@@ -0,0 +1,14 @@
+﻿namespace Test
+{
+    internal class SpaceShip
+    {
+        public SpaceShip()
+        {
+        }
+
+        internal void location(byte x, byte y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
