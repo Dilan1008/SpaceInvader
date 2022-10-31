110d5b8f859bf639d810bcc87f18e09a7e481bc0
Dilan1008 <107607645+Dilan1008@users.noreply.github.com>
2022-10-31 11:21:43 +0100

    première story
    Faire valider la finition de la première story

--- SpaceInvaders/SpaceInvaders/Program.cs
+++ SpaceInvaders/SpaceInvaders/Program.cs
@@ -1,86 +1,110 @@
 ﻿List<int> myList = new List<int>();
-string stgArrow = " -->";
+string stgArrow = "-->";
 int bytX = 40;
-int bytY = 11;
-ConsoleKey Key;
+int bytY = 10;
+byte Enter;
 
 do
 {
-
-    Console.Clear();
-
-    Console.WriteLine("\t ███████ ██████   █████   ██████ ███████     ██ ███    ██ ██    ██  █████  ██████  ███████ ██████  ███████ ");
-    Console.WriteLine("\t ██      ██   ██ ██   ██ ██      ██          ██ ████   ██ ██    ██ ██   ██ ██   ██ ██      ██   ██ ██      ");
-    Console.WriteLine("\t ███████ ██████  ███████ ██      █████       ██ ██ ██  ██ ██    ██ ███████ ██   ██ █████   ██████  ███████ ");
-    Console.WriteLine("\t      ██ ██      ██   ██ ██      ██          ██ ██  ██ ██  ██  ██  ██   ██ ██   ██ ██      ██   ██      ██ ");
-    Console.WriteLine("\t ███████ ██      ██   ██  ██████ ███████     ██ ██   ████   ████   ██   ██ ██████  ███████ ██   ██ ███████ ");
-    Console.WriteLine("\n\n");
-
-    Console.WriteLine("\t\t\t\t\t\t" + @"  ____  _             ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |  _ \| | __ _ _   _ ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" | |_) | |/ _` | | | |");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |  __/| | (_| | |_| |");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |_|   |_|\__,_|\__, |");
-    Console.WriteLine("\t\t\t\t\t\t" + @"                |___/");
-
-    Console.WriteLine("\t\t\t\t\t\t" + @"  ____       _   _   _                 ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" / ___|  ___| |_| |_(_)_ __   __ _ ___ ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" \___ \ / _ \ __| __| | '_ \ / _` / __|");
-    Console.WriteLine("\t\t\t\t\t\t" + @"  ___) |  __/ |_| |_| | | | | (_| \__ \");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |____/ \___|\__|\__|_|_| |_|\__, |___/");
-    Console.WriteLine("\t\t\t\t\t\t" + @"                             |___/     ");
-
-    Console.WriteLine("\t\t\t\t\t\t" + @"  _   _ _       _                                 ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" | | | (_) __ _| |__  ___  ___ ___  _ __ ___  ___ ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" | |_| | |/ _` | '_ \/ __|/ __/ _ \| '__/ _ \/ __|");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |  _  | | (_| | | | \__ \ (_| (_) | | |  __/\__ \");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |_| |_|_|\__, |_| |_|___/\___\___/|_|  \___||___/");
-    Console.WriteLine("\t\t\t\t\t\t" + @"          |___/                                   ");
-
-    Console.WriteLine("\t\t\t\t\t\t" + @"     _    _                 _   ");
-    Console.WriteLine("\t\t\t\t\t\t" + @"    / \  | |__   ___  _   _| |_ ");
-    Console.WriteLine("\t\t\t\t\t\t" + @"   / _ \ | '_ \ / _ \| | | | __|");
-    Console.WriteLine("\t\t\t\t\t\t" + @"  / ___ \| |_) | (_) | |_| | |_ ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" /_/   \_\_.__/ \___/ \__,_|\__|");
-
-    Console.WriteLine("\t\t\t\t\t\t" + @"  _____      _ _   ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" | ____|_  _(_) |_ ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |  _| \ \/ / | __|");
-    Console.WriteLine("\t\t\t\t\t\t" + @" | |___ >  <| | |_ ");
-    Console.WriteLine("\t\t\t\t\t\t" + @" |_____/_/\_\_|\__|");
-
-    Console.SetCursorPosition(3, 3);
-    bytY += myList.Count;
-    Console.SetCursorPosition(bytX, (myList.Count + bytY));
-    Console.Write(stgArrow);
-
-
-    switch (Console.ReadKey().Key)
+    do
     {
-        case ConsoleKey.UpArrow:
+        Console.Clear();
+        
+        Console.WriteLine("\t ███████ ██████   █████   ██████ ███████     ██ ███    ██ ██    ██  █████  ██████  ███████ ██████  ███████ ");
+        Console.WriteLine("\t ██      ██   ██ ██   ██ ██      ██          ██ ████   ██ ██    ██ ██   ██ ██   ██ ██      ██   ██ ██      ");
+        Console.WriteLine("\t ███████ ██████  ███████ ██      █████       ██ ██ ██  ██ ██    ██ ███████ ██   ██ █████   ██████  ███████ ");
+        Console.WriteLine("\t      ██ ██      ██   ██ ██      ██          ██ ██  ██ ██  ██  ██  ██   ██ ██   ██ ██      ██   ██      ██ ");
+        Console.WriteLine("\t ███████ ██      ██   ██  ██████ ███████     ██ ██   ████   ████   ██   ██ ██████  ███████ ██   ██ ███████ ");
+        Console.WriteLine("\n\n");
 
-            bytY = bytY - 5;
-            break;
+        Console.WriteLine("\t\t\t\t\t\t" + @"  ____  _             ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |  _ \| | __ _ _   _ ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" | |_) | |/ _` | | | |");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |  __/| | (_| | |_| |");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |_|   |_|\__,_|\__, |");
+        Console.WriteLine("\t\t\t\t\t\t" + @"                |___/");
 
-        case ConsoleKey.DownArrow:
+        Console.WriteLine("\t\t\t\t\t\t" + @"  ____       _   _   _                 ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" / ___|  ___| |_| |_(_)_ __   __ _ ___ ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" \___ \ / _ \ __| __| | '_ \ / _` / __|");
+        Console.WriteLine("\t\t\t\t\t\t" + @"  ___) |  __/ |_| |_| | | | | (_| \__ \");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |____/ \___|\__|\__|_|_| |_|\__, |___/");
+        Console.WriteLine("\t\t\t\t\t\t" + @"                             |___/     ");
 
-            bytY = bytY + 6;
-            if (bytY == 17)
-            {
-                bytY = bytY + 7;
-            }
-            break;
+        Console.WriteLine("\t\t\t\t\t\t" + @"  _   _ _       _                                 ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" | | | (_) __ _| |__  ___  ___ ___  _ __ ___  ___ ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" | |_| | |/ _` | '_ \/ __|/ __/ _ \| '__/ _ \/ __|");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |  _  | | (_| | | | \__ \ (_| (_) | | |  __/\__ \");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |_| |_|_|\__, |_| |_|___/\___\___/|_|  \___||___/");
+        Console.WriteLine("\t\t\t\t\t\t" + @"          |___/                                   ");
 
-        case ConsoleKey.Enter:
-            Console.SetCursorPosition(0, myList.Count + 8);
-            Console.Write("nouvelle course : ");
-            Console.SetCursorPosition(0, myList.Count + 10);
-            Console.Write("niveau d'importance : ");
+        Console.WriteLine("\t\t\t\t\t\t" + @"     _    _                 _   ");
+        Console.WriteLine("\t\t\t\t\t\t" + @"    / \  | |__   ___  _   _| |_ ");
+        Console.WriteLine("\t\t\t\t\t\t" + @"   / _ \ | '_ \ / _ \| | | | __|");
+        Console.WriteLine("\t\t\t\t\t\t" + @"  / ___ \| |_) | (_) | |_| | |_ ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" /_/   \_\_.__/ \___/ \__,_|\__|");
 
-            break;
+        Console.WriteLine("\t\t\t\t\t\t" + @"  _____      _ _   ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" | ____|_  _(_) |_ ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |  _| \ \/ / | __|");
+        Console.WriteLine("\t\t\t\t\t\t" + @" | |___ >  <| | |_ ");
+        Console.WriteLine("\t\t\t\t\t\t" + @" |_____/_/\_\_|\__|");
 
-    }
-} while (true);
+        /*Console.SetCursorPosition(0, 3);
+        Console.SetCursorPosition(0, 3);
+        bytY += myList.Count;*/
+        Console.SetCursorPosition(bytX, (myList.Count + bytY));
+        Console.Write(stgArrow);
+
+        Enter = 0;
+
+        switch (Console.ReadKey().Key)
+        {
+            case ConsoleKey.UpArrow:
+
+                bytY = bytY - 6;
+                if (bytY == 29)
+                {
+                    bytY = bytY + 1;
+                }
+                if (bytY < 10)
+                {
+                    bytY = 10;
+                }
+                break;
+
+            case ConsoleKey.DownArrow:
+
+                bytY = bytY + 6;
+                if (bytY == 29)
+                {
+                    bytY = bytY - 1;
+                }
+                if (bytY > 34)
+                {
+                    bytY = 34;
+                }
+                break;
+
+            case ConsoleKey.Enter:
+
+                Enter = 1;
+                break;
+
+        }
+    } while (Enter != 1);
+
+    do
+    {
+        if(bytY == 10)
+        {
+            Console.Clear();
+            Console.WriteLine("Vous êtes dans le jeux");
+        }
+
+    } while (Console.ReadKey().Key != ConsoleKey.Enter);
+
+}while (true);
 
 
 
